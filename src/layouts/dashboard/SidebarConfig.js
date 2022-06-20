10be3ed0e54631e842b7be3827a780a67a09c0bc
Bennny20 <phientruong20@gmail.com>
2022-06-20 22:30:21 +0700

    first commit

---
+++ src/layouts/dashboard/SidebarConfig.js
@@ -0,0 +1,112 @@
+// routes
+import { PATH_DASHBOARD } from '../../routes/paths';
+// components
+import Label from '../../components/Label';
+import SvgIconStyle from '../../components/SvgIconStyle';
+
+// ----------------------------------------------------------------------
+
+const getIcon = (name) => (
+  <SvgIconStyle src={`/static/icons/navbar/${name}.svg`} sx={{ width: '100%', height: '100%' }} />
+);
+
+const ICONS = {
+  blog: getIcon('ic_blog'),
+  cart: getIcon('ic_cart'),
+  chat: getIcon('ic_chat'),
+  mail: getIcon('ic_mail'),
+  user: getIcon('ic_user'),
+  kanban: getIcon('ic_kanban'),
+  banking: getIcon('ic_banking'),
+  calendar: getIcon('ic_calendar'),
+  ecommerce: getIcon('ic_ecommerce'),
+  analytics: getIcon('ic_analytics'),
+  dashboard: getIcon('ic_dashboard'),
+  booking: getIcon('ic_booking')
+};
+
+const sidebarConfig = [
+  // GENERAL
+  // ----------------------------------------------------------------------
+  {
+    subheader: 'general',
+    items: [
+      {
+        title: 'Math',
+        path: PATH_DASHBOARD.general.booking,
+        icon: ICONS.dashboard
+      }
+    ]
+  },
+
+  // Account & User
+  // ----------------------------------------------------------------------
+  {
+    subheader: 'Account & User',
+    items: [
+      // MANAGEMENT : USER
+      {
+        title: 'user',
+        path: PATH_DASHBOARD.user.root,
+        icon: ICONS.user,
+        children: [
+          { title: 'account', path: PATH_DASHBOARD.user.account },
+          { title: 'list user', path: PATH_DASHBOARD.user.list }
+        ]
+      }
+
+      // MANAGEMENT : E-COMMERCE
+      // {
+      //   title: 'e-commerce',
+      //   path: PATH_DASHBOARD.eCommerce.root,
+      //   icon: ICONS.cart,
+      //   children: [
+      //     { title: 'shop', path: PATH_DASHBOARD.eCommerce.shop },
+      //     { title: 'product', path: PATH_DASHBOARD.eCommerce.productById },
+      //     { title: 'list', path: PATH_DASHBOARD.eCommerce.list },
+      //     { title: 'create', path: PATH_DASHBOARD.eCommerce.newProduct },
+      //     { title: 'edit', path: PATH_DASHBOARD.eCommerce.editById },
+      //     { title: 'checkout', path: PATH_DASHBOARD.eCommerce.checkout },
+      //     { title: 'invoice', path: PATH_DASHBOARD.eCommerce.invoice }
+      //   ]
+      // },
+
+      // MANAGEMENT : BLOG
+      // {
+      //   title: 'blog',
+      //   path: PATH_DASHBOARD.blog.root,
+      //   icon: ICONS.blog,
+      //   children: [
+      //     { title: 'posts', path: PATH_DASHBOARD.blog.posts },
+      //     { title: 'post', path: PATH_DASHBOARD.blog.postById },
+      //     { title: 'new post', path: PATH_DASHBOARD.blog.newPost }
+      //   ]
+      // }
+    ]
+  },
+
+  // Manager
+  // ----------------------------------------------------------------------
+  {
+    subheader: 'Manager',
+    items: [
+      {
+        title: 'Club',
+        path: PATH_DASHBOARD.mail.root,
+        icon: ICONS.mail
+      },
+      {
+        title: 'Stadium',
+        path: PATH_DASHBOARD.mail.root,
+        icon: ICONS.mail
+      },
+      {
+        title: 'Math',
+        path: PATH_DASHBOARD.mail.root,
+        icon: ICONS.mail
+      }
+    ]
+  }
+];
+
+export default sidebarConfig;
